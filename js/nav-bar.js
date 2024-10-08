d7659d16a0f9e35d8994e7504667b833c40b18dd
Brandon Cortes Islas <itsolutions.utn@gmail.com>
2024-10-08 17:36:00 -0600

    feat: Integración del próximamente

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,33 +1,31 @@
-import { resaltaSiEstasEn } from "../lib/js/resaltaSiEstasEn.js"
+import { resaltaSiEstasEn } from "../lib/js/resaltaSiEstasEn.js";
 
 export class NavBar extends HTMLElement {
+	connectedCallback() {
+		this.classList.add("md-navigation-bar");
 
- connectedCallback() {
-  this.classList.add("md-navigation-bar")
+		this.innerHTML = /* HTML */ `
+			<a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
+				<span class="material-symbols-outlined">bolt</span>
+				Próximamente
+			</a>
 
-  this.innerHTML = /* HTML */`
-   <a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
-    <span class="material-symbols-outlined">home</span>
-    Inicio
-   </a>
+      <a ${resaltaSiEstasEn(["/formulario.html"])} href="proximamente.html">
+				<span class="material-symbols-outlined">newspaper</span>
+				Forma
+			</a>
 
-   <a ${resaltaSiEstasEn(["/navTabFixed.html"])} href="navTabFixed.html">
-    <span class="material-symbols-outlined">tabs</span>
-    Pestañas
-   </a>
+			<a ${resaltaSiEstasEn(["/formulario.html"])} href="formulario.html">
+				<span class="material-symbols-outlined">newspaper</span>
+				Forma
+			</a>
 
-   <a id="navbar" ${resaltaSiEstasEn(["/navbar.html"])} href="navbar.html">
-    <span class="material-symbols-outlined">bottom_navigation</span>
-    Barra
-   </a>
-
-   <a ${resaltaSiEstasEn(["/formulario.html"])} href="formulario.html">
-    <span class="material-symbols-outlined">newspaper</span>
-    Forma
-   </a>`
-
- }
-
+      <a ${resaltaSiEstasEn(["/ayuda.html"])} href="ayuda.html">
+				<span class="material-symbols-outlined">help</span>
+				Ayuda
+			</a>
+		`;
+	}
 }
 
-customElements.define("nav-bar", NavBar)
+customElements.define("nav-bar", NavBar);

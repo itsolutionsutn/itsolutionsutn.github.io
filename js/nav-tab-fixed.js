97041c54b173f9ba646c29a8196bd74093a26774
Brandon Cortes Islas <itsolutions.utn@gmail.com>
2024-10-08 17:49:02 -0600

    fix: Navbar index

--- js/nav-tab-fixed.js
+++ js/nav-tab-fixed.js
@@ -1,33 +1,31 @@
-import { resaltaSiEstasEn } from "../lib/js/resaltaSiEstasEn.js"
+import { resaltaSiEstasEn } from "../lib/js/resaltaSiEstasEn.js";
 
 export class NavTabFixed extends HTMLElement {
+	connectedCallback() {
+		this.classList.add("md-tab", "fixed");
 
- connectedCallback() {
-  this.classList.add("md-tab", "fixed")
+		this.innerHTML = /* HTML */ `
+			<a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
+				<span class="material-symbols-outlined">home</span>
+				Inicio
+			</a>
 
-  this.innerHTML = /* HTML */`
-   <a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
-    <span class="material-symbols-outlined">home</span>
-    Inicio
-   </a>
+			<a ${resaltaSiEstasEn(["/proximamente.html"])} href="proximamente.html">
+				<span class="material-symbols-outlined">bolt</span>
+				Próximamente
+			</a>
 
-   <a ${resaltaSiEstasEn(["/navtab.html"])} href="navtab.html">
-    <span class="material-symbols-outlined">swipe_left</span>
-    Pestañas scrollable
-   </a>
+			<a id="navtabfixed" ${resaltaSiEstasEn(["/formulario.html"])} href="formulario.html">
+				<span class="material-symbols-outlined">newspaper</span>
+				Formulario
+			</a>
 
-   <a id="navtabfixed" ${resaltaSiEstasEn(["/navTabFixed.html"])}
-     href="navTabFixed.html">
-    <span class="material-symbols-outlined">tabs</span>
-    Pestañas fijas
-   </a>
-
-   <a ${resaltaSiEstasEn(["/navbar.html"])} href="navbar.html">
-    <span class="material-symbols-outlined">bottom_navigation</span>
-    Barra de navegación
-   </a>`
- }
-
+			<a ${resaltaSiEstasEn(["/ayuda.html"])} href="ayuda.html">
+				<span class="material-symbols-outlined">help</span>
+				Ayuda
+			</a>
+		`;
+	}
 }
 
-customElements.define("nav-tab-fixed", NavTabFixed)
+customElements.define("nav-tab-fixed", NavTabFixed);

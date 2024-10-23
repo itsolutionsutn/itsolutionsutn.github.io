5d3a1c76f5f7b2ce1021e06bbe64eee3d362221f
Brandon Cortes Islas <itsolutions.utn@gmail.com>
2024-10-23 01:01:47 -0600

    prueba

---
+++ rendercli/srv/lista.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__ . "/../lib/php/devuelveJson.php";
+
+devuelveJson([
+ [
+  "nombre" => "pp",
+  "color" => "azul"
+ ],
+ [
+  "nombre" => "kq",
+  "color" => "rojo"
+ ],
+ [
+  "nombre" => "tt",
+  "color" => "rosa"
+ ],
+ [
+  "nombre" => "bb",
+  "color" => "azul"
+ ]
+]);

829b4a9b8135fd3fae0052abb6e52b61a0800e60
Brandon Cortes Islas <itsolutions.utn@gmail.com>
2024-10-23 07:31:10 -0600

    Fix:Navbar

--- js/nav-tab-fixed.js
+++ js/nav-tab-fixed.js
@@ -10,14 +10,19 @@ export class NavTabFixed extends HTMLElement {
 				Inicio
 			</a>
 
-			<a id="navtabfixed" ${resaltaSiEstasEn(["/two-line.html"])} href="two-line.html">
-				<span class="material-symbols-outlined">tabs</span>
-				Pestañas fijas
+			<a id="navtabfixed" ${resaltaSiEstasEn(["/renderCliente.html"])} href="renderCliente.html">
+				<span class="material-symbols-outlined">list</span>
+				Lista de Chistes C
+			</a>
+			
+			<a id="navtabfixed" ${resaltaSiEstasEn(["/renderServidor.html"])} href="renderServidor.html">
+				<span class="material-symbols-outlined">list</span>
+				Lista de Chistes S
 			</a>
 
 			<a ${resaltaSiEstasEn(["/ayuda.html"])} href="ayuda.html">
-				<span class="material-symbols-outlined">bottom_navigation</span>
-				Barra de navegación
+				<span class="material-symbols-outlined">help</span>
+				Ayuda
 			</a>
 		`;
 	}

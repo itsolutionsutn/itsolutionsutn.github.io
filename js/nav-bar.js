1cc828fbc59f5769e0b8ead6767100692180b6c3
Brandon Cortes Islas <itsolutions.utn@gmail.com>
2024-10-22 21:23:21 -0600

    Revert "fix: interfaz chistes"
    This reverts commit b76d4ab72f010dd9a31248a9a29d48bdd839389d.

--- js/nav-bar.js
+++ js/nav-bar.js
@@ -1,31 +1,33 @@
-import { resaltaSiEstasEn } from "../lib/js/resaltaSiEstasEn.js";
+import { resaltaSiEstasEn } from "../lib/js/resaltaSiEstasEn.js"
 
 export class NavBar extends HTMLElement {
-	connectedCallback() {
-		this.classList.add("md-navigation-bar");
 
-		this.innerHTML = /* HTML */ `
-			<a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
-				<span class="material-symbols-outlined">home</span>
-				Inicio
-			</a>
+ connectedCallback() {
+  this.classList.add("md-navigation-bar")
 
-			<a ${resaltaSiEstasEn(["/navTabFixed.html"])} href="navTabFixed.html">
-				<span class="material-symbols-outlined">tabs</span>
-				Pestañas
-			</a>
+  this.innerHTML = /* HTML */`
+   <a ${resaltaSiEstasEn(["/index.html", "", "/"])} href="index.html">
+    <span class="material-symbols-outlined">home</span>
+    Inicio
+   </a>
 
-			<a id="navbar" ${resaltaSiEstasEn(["/navbar.html"])} href="navbar.html">
-				<span class="material-symbols-outlined">bottom_navigation</span>
-				Barra
-			</a>
+   <a ${resaltaSiEstasEn(["/navTabFixed.html"])} href="navTabFixed.html">
+    <span class="material-symbols-outlined">tabs</span>
+    Pestañas
+   </a>
 
-			<a ${resaltaSiEstasEn(["/formulario.html"])} href="formulario.html">
-				<span class="material-symbols-outlined">newspaper</span>
-				Forma
-			</a>
-		`;
-	}
+   <a id="navbar" ${resaltaSiEstasEn(["/navbar.html"])} href="navbar.html">
+    <span class="material-symbols-outlined">bottom_navigation</span>
+    Barra
+   </a>
+
+   <a ${resaltaSiEstasEn(["/formulario.html"])} href="formulario.html">
+    <span class="material-symbols-outlined">newspaper</span>
+    Forma
+   </a>`
+
+ }
+
 }
 
-customElements.define("nav-bar", NavBar);
+customElements.define("nav-bar", NavBar)
